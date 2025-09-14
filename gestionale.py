02d459aeb10ae4f9dfe47432a296c5a264b753d3
Letterio23 <167696315+Letterio23@users.noreply.github.com>
2025-09-14 11:56:16 +0200

    gestionale.py

--- gestionale.py
+++ gestionale.py
@@ -71,6 +71,7 @@ def sorare_graphql_fetch(query, variables={}):
         "Accept": "application/json",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.9"
+		"X-Sorare-ApiVersion": "v1"
     }
     # ----------------------------------------------------
     try:
